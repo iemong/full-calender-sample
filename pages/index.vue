22ebecbc091507da00667141d58feab4333e20ea
iemong <kiyoshidainagon@gmail.com>
2020-11-03 17:13:54 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div
+    p.text-red-700 {{ message }}
+    button(class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded') button
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const message = ref('This is a message')
+
+    return {
+      message
+    }
+  }
+})
+</script>
+<style lang="stylus"></style>
